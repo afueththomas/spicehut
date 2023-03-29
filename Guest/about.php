b4706b64b46f2e65398ca85046642478e6e0c9ed
afueththomas <afueththomas2023b@mca.ajce.in>
2023-03-29 11:58:24 +0530

    Updated

--- Guest/about.php
+++ Guest/about.php
@@ -44,22 +44,28 @@ include("header.php");
       <h1>About Us</h1>
       <img class="logo" src="images/logo-2.png" alt="Spice Shop Logo" />
       <p>
+
         Welcome to SpiceHut, your one-stop online destination for all your
         spice needs. Our mission is to provide high-quality, authentic spices
         from around the world to spice lovers everywhere. From exotic blends to
         everyday seasonings, we've got you covered.
+
       </p>
       <p>
+
         Our team of spice experts carefully sources and selects each and every
         spice we offer to ensure the highest quality and freshest flavors. We
         believe that cooking with fresh, high-quality spices is the key to
         unlocking the full potential of any dish.
+
       </p>
       <p>
+
         Whether you're an experienced chef or a home cook just starting out,
         we've got the spices you need to take your cooking to the next level.
         Browse our selection of spices today and discover the flavors of the
         world.
+        
       </p>
     </div>
     </form>
